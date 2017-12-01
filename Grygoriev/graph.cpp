09f8f59d45cbf179ac792ed0116817f729ea0e98
Ustim93 <Yustim@ukr.net>
2017-12-01 17:27:03 +0200

    Update graph.cpp

--- Grygoriev/graph.cpp
+++ Grygoriev/graph.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 const int SIZE = 6;
@@ -6,19 +7,40 @@ const int INF = 1000000000;
 
 void dijkstra(int a[SIZE][SIZE])
 {
-
+  queue<int> Queue;
   int distance[SIZE]; // масив найкоротших шляхів 
-  bool vertex[SIZE]; // массив міток (чи відвідана дана вершина)?
+  bool vertex[SIZE]; // массив міток (чи відвідана дана вершина?)
   
   int temp; // для временного хранения пути
   int minindex, dist;
 
   for (i = 0; i<SIZE; i++) {
-    distance[i] = INF; // массив путей инициализируется бесконечностью
-    vertex[i] = 1;
+    distance[i] = INF; 
+    vertex[i] = 0;
   }
   
+  Queue.push(0); // поміщаємо в чергу першу вершину
+  
   d[0] = 0;
+  
+  while (!Queue.empty())
+  {
+    int node = Queue.front(); 
+    Queue.pop();
+    vertex[node] = 1; // відмічаємо її як таку, що віввідана
+    for (int j = 0; j < 7; j++)
+    {
+      if (mas[node][j] == 1 && nodes[j] == 0)
+      { // если вершина смежная и не обнаружена
+        Queue.push(j); // добавляем ее в очередь
+        nodes[j] = 1; // отмечаем вершину как обнаруженную
+        e.begin = node; e.end = j;
+        Edges.push(e);
+        if (node == req) break;
+      }
+    }
+    cout << node + 1 << endl; // выводим номер вершины
+  }
   
   do
   { // исполнение алгоритма 
