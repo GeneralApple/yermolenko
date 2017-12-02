57542b7251b8a7a7aa4022b92fdd8334c77fcf79
Ustim93 <Yustim@ukr.net>
2017-12-03 02:43:07 +0300

    Update lambda.cpp

--- Grygoriev/lambda.cpp
+++ Grygoriev/lambda.cpp
@@ -1,29 +1,29 @@
-
 #include "stdafx.h"
 #include <iostream>
-#include <time.h> 
+#include <ctime>
 #include <cstdlib>
 #include <algorithm>
 
 using namespace std;
 
-int main
+int main()
 {
-	srand(time(0));
-int rnd0 = 1 + rand() / 100;
-int arr[10];
+	srand(time(0)); // автоматическая рандомизация
+	int rnd0 = 1 + rand() % 100;
 
-for (int i = 0; i<100; i++)
-{
-	arr[i] = i;
-}
+	cout << rnd0;
+	int arr[10];
 
-for_each(arr, arr + 100, [](int n) {
-	unsigned long int rnd = rnd0* (1664525 << n) + 1013904223UL;
-	cout << rnd<<endl;
-});
+	for (int i = 0; i<100; i++)
+	{
+		arr[i] = i;
+	}
 
-int pause;
-cin >> pause;
-return 0;
+	for_each(arr, arr + 100, [rnd0](int n) {
+		unsigned long int rnd = 1+ (rnd0* (1664525 << n) + 1013904223UL)%100;
+		cout << rnd << endl;
+	});
+
+	system("pause");
+	return 0;
 }
