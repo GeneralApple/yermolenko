16c22d752cb09b4643d0d0347ae5323993016dc9
Ustim93 <Yustim@ukr.net>
2017-12-03 01:57:27 +0300

    Update lambda.cpp

--- Grygoriev/lambda.cpp
+++ Grygoriev/lambda.cpp
@@ -2,11 +2,17 @@
 #include "stdafx.h"
 #include <iostream>
 #include <time.h> 
+#include <cstdlib>
 
 using namespace std;
 
 int main
 {
+srand(time(NULL));
+int t = 1 + rand()/100
 
+
+int pause;
+cin>>pause;
 return 0;
 }
