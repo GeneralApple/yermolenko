3c70f2e2177d98214084e36e28e022f3b0e3df9f
Ustim93 <Yustim@ukr.net>
2017-12-03 02:22:35 +0300

    Update lambda.cpp

--- Grygoriev/lambda.cpp
+++ Grygoriev/lambda.cpp
@@ -10,13 +10,16 @@ using namespace std;
 int main
 {
 srand(time(NULL));
-int t = 1 + rand()/100;
-int arr[100];
+ int rnd0 = 1 + rand()/100;
+int arr[32];
 
 for(int i = 0; i<100;i++)
 {arr[i] = i;}
 
-
+for_each(arr, arr+100, [](int n) {
+unsigned long int rnd = rnd0* (1664525<<n) + 1013904223UL;
+cout<<rnd;
+});
 
 int pause;
 cin>>pause;
