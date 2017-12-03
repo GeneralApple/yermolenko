9c895df99df677088ed8f4e94f4ae4f730131d3a
Ustim93 <Yustim@ukr.net>
2017-12-03 03:48:34 +0300

    Update lambda2.cpp

--- Grygoriev/lambda2.cpp
+++ Grygoriev/lambda2.cpp
@@ -1,4 +1,4 @@
-//#include "stdafx.h"
+#include "stdafx.h"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -9,20 +9,20 @@ using namespace std;
 
 int main()
 {
-	
-	srand(time(0)); 
-	unsigned long int rnd = 1+rand()%100;
-	cout<<rnd<<endl;
-	
-	unsigned long int arr[10]
-	
-	for (int i = 0; i<10; i++)
-	{
-	rnd = rnd * 1664525 + 1013904223UL;
-	cout<<1+rnd%100<<endl;
-	}
-	
 
-	
+	srand(time(0));
+	unsigned long int rnd = 1 + rand() % 100;
+	cout << rnd << endl;
+
+	unsigned long int arr[1000];
+
+		for (int i = 0; i<1000; i++)
+		{
+			rnd = rnd * 1664525 + 1013904223UL;
+			cout << 1 + rnd % 100 << endl;
+		}
+
+		system("pause");
+
 	return 0;
 }
