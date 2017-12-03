1091132b26f00a21e4dda33523392d6d5b379e80
Ustim93 <Yustim@ukr.net>
2017-12-03 05:41:40 +0300

    Update lambda.cpp

--- Grygoriev/lambda.cpp
+++ Grygoriev/lambda.cpp
@@ -11,21 +11,25 @@ int main()
 	srand(time(0)); // автоматическая рандомизация
 	int rnd0 = 1 + rand() % 100;
 
-	cout << rnd0;
+	//cout << rnd0;
 	int arr[1000];
 
+	for(int i=0;i<1000;i++) arr[i]=i;
+	
+	int rnd[1000]
+	
 	
 
-	for_each(arr, arr + 1000, [rnd0,&arr](int n) {
-		unsigned long int rnd = rnd0 * 1664525*n + 1013904223UL*n+1;
-		arr[n-1] = 1+rnd%100
-		//cout << 1+rnd%100 << endl;
+	for_each(arr, arr + 1000, [rnd0,&rnd](int n) {
+		unsigned long int tmp = rnd0 * (1+1664524*n) + 1013904223UL*n;
+		rnd[n] = 1+tmp%100;
+		//cout << 1+tmp%100 << endl;
 	});
 	
 		for (int j = 0; j < 10; j++)
 	{
-		int result = count_if(arr, arr + 1000, [j,arr](int n) {
-			return 10*j <= arr[n] && arr[n] < 10*(j+1);
+		int result = count_if(rnd, rnd + 1000, [j](int n) {
+			return 10*j <= n && n < 10*(j+1);
 		});
 		cout << result << "     ";
 	}
