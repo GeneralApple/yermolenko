a170b73e9e98fbbcd3d58825f52ef7eaa66af745
Ustim93 <Yustim@ukr.net>
2017-12-03 21:12:18 +0300

    Update model.cu

--- Grygoriev/GPU/model.cu
+++ Grygoriev/GPU/model.cu
@@ -14,14 +14,27 @@ int main() {
 
 int N;
 cin >> N;
-double mu=0.07,l,dTh;
+ 
+double mu=0.07;
 
 double h = 100;
+  
+  int *dN;
+  
+  double *dmu, *dh;
+  
+  cudaMalloc((void**)&dN, sizeof(int));
+  cudaMalloc((void**)&dmu, sizeof(double));
+  cudaMalloc((void**)&dh, sizeof(double));
+  
+   cudaMemcpy(dN, &N, sizeof(int), cudaMemcpyHostToDevice);
+   cudaMemcpy(dmu, &mu, sizeof(double), cudaMemcpyHostToDevice);
+     cudaMemcpy(dh, &h, sizeof(double), cudaMemcpyHostToDevice);
+  
 
-srand(time(NULL));
 
-double position[N];
+  
 
-int *N_gpu;
+//double position[N];
 
-do
+
