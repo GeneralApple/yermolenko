d4312cb524e7511e74a9690a0b61deef5e10bfb7
Ustim93 <Yustim@ukr.net>
2017-12-03 03:56:46 +0300

    Update lambda2.cpp

--- Grygoriev/lambda2.cpp
+++ Grygoriev/lambda2.cpp
@@ -14,13 +14,20 @@ int main()
 	unsigned long int rnd = 1 + rand() % 100;
 	cout << rnd << endl;
 
-	unsigned long int arr[1000];
+	unsigned long int arr[100000];
 
-		for (int i = 0; i<1000; i++)
+		for (int i = 0; i<100000; i++)
 		{
 			rnd = rnd * 1664525 + 1013904223UL;
-			cout << 1 + rnd % 100 << endl;
+			arr[i] = 1 + rnd % 100;
+			//cout << 1 + rnd % 100 << endl;
 		}
+	
+	for(int j = 0; j < 100; j++)
+	{
+	int result = count_if(arr, arr + 100000, [j](int n) {
+		return j<=n && n< j+ 1000;
+	});
 
 		system("pause");
 
