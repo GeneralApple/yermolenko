9f40a7ae9a92326c6145751c70133f1c72899962
Ustim93 <Yustim@ukr.net>
2017-12-03 05:10:01 +0300

    Update lambda.cpp

--- Grygoriev/lambda.cpp
+++ Grygoriev/lambda.cpp
@@ -12,17 +12,23 @@ int main()
 	int rnd0 = 1 + rand() % 100;
 
 	cout << rnd0;
-	int arr[10];
+	int arr[1000];
 
-	for (int i = 0; i<10; i++)
-	{
-		arr[i] = i;
-	}
+	
 
-	for_each(arr, arr + 10, [rnd0](int n) {
-		unsigned long int rnd = rnd0 * 1664525*(n+1) + 1013904223UL*(n+1);
-		cout << 1+rnd%100 << endl;
+	for_each(arr, arr + 1000, [rnd0,&arr](int n) {
+		unsigned long int rnd = rnd0 * 1664525*n + 1013904223UL*n+1;
+		arr[n-1] = 1+rnd%100
+		//cout << 1+rnd%100 << endl;
 	});
+	
+		for (int j = 0; j < 10; j++)
+	{
+		int result = count_if(arr, arr + 1000, [j,arr](int n) {
+			return 10*j <= arr[n] && arr[n] < 10*(j+1);
+		});
+		cout << result << "     ";
+	}
 
 	system("pause");
 	return 0;
