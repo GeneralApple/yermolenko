6b608242527731b8b9ef2f809fe18f2e7cc4d478
Ustim93 <Yustim@ukr.net>
2017-12-05 01:12:52 +0300

    Update astar.cpp

--- Grygoriev/astar.cpp
+++ Grygoriev/astar.cpp
@@ -3,4 +3,35 @@
 #include <queue>
 using namespace std;
 
+const int INF = 1000000000;
+
+struct game
+{ 
+  //x та y- координати
+  int x,y;
+  bool up; //1 - клітинка відкрита, 0 - закрита (стіна)
+  int pointer; //вказівник на попередню клітинку
+  int dist; //шлях до клітини
+  bool vertex; // массив міток (чи відвідана дана rклітина?)
+  bool nodes; //відмічає виявлені (додані в чергу) клітини
+  
+}
+
 int main{
+  
+  game a[25];
+  
+  for(int i = 0; i<25; i++) a[i].x = i%5 + 1;
+  for(int i = 0; i<25; i++) a[i].y = i/5 + 1;
+  
+  for(int i = 0; i<25; i++) a[i].up = 1;
+  
+  //малюємо стіни
+  a[8].up = 0;
+  a[13].up = 0;
+  a[17].up = 0;
+  a[18].up = 0;
+  
+  
+ 
+ 
