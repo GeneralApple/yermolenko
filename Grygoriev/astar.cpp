13ecb37c5abe6a190f0d0ce4cb1648bc3113f1e5
Ustim93 <Yustim@ukr.net>
2017-12-05 01:23:05 +0300

    Update astar.cpp

--- Grygoriev/astar.cpp
+++ Grygoriev/astar.cpp
@@ -6,7 +6,8 @@ using namespace std;
 const int INF = 1000000000;
 
 struct game
-{ 
+{  
+  //int num;// номер клітини
   //x та y- координати
   int x,y;
   bool up; //1 - клітинка відкрита, 0 - закрита (стіна)
@@ -21,16 +22,29 @@ int main{
   
   game a[25];
   
+  //for(int i = 0; i<25; i++) a[i].num = i+1;
+  
   for(int i = 0; i<25; i++) a[i].x = i%5 + 1;
   for(int i = 0; i<25; i++) a[i].y = i/5 + 1;
   
   for(int i = 0; i<25; i++) a[i].up = 1;
+  for(int i = 0; i<25; i++) a[i].vertex = 0;
   
   //малюємо стіни
   a[8].up = 0;
   a[13].up = 0;
   a[17].up = 0;
   a[18].up = 0;
+  
+  int init, end;
+  cout << "Enter start point:";
+  cin >> init;
+   init--;
+  
+  a[init].pointer = 0;
+  a[init].vertex = 1;
+  
+   a[init].dist = 0;
   
   
  
