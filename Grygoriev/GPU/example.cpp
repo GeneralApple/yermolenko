5d5620ebde429d237818390c4124484ef8af629b
Ustim93 <Yustim@ukr.net>
2017-12-18 23:07:18 +0300

    Update example.cpp

--- Grygoriev/GPU/example.cpp
+++ Grygoriev/GPU/example.cpp
@@ -1,9 +1,25 @@
 #include <thread>  
 #include <iostream>
+
+#include <unistd.h>
+
 using namespace std;
+
+void foo
+{
+  for(int i=0; i<10; i++)
+  {
+    cout<<i<<" done"<<endl;
+    sleep(1)
+    }
+ }
 
 int main(){
 
+thread t(foo)
 
+t.detach;
+
+system("pause");
 return 0;
 }
