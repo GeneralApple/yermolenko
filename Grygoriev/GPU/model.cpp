a5855c0e8093a7e9f1260b302d8a4f2c73cdad08
Ustim93 <Yustim@ukr.net>
2017-12-19 16:27:09 +0300

    Update model.cpp

--- Grygoriev/GPU/model.cpp
+++ Grygoriev/GPU/model.cpp
@@ -1,15 +1,31 @@
 #include <thread>  
 #include <iostream>
 #include <time.h>
+#include <cmath>
+#include <cstdlib>
 //#include <unistd.h>
 
 using namespace std;
+
+void model(double d;
 
 
 int main() {
 
 	srand(time(0));
-
+	int N = 1000;
+	double d; //товщина пластинки
+        cin >> d;
+        double mu=0.1; //довжина вільного пробігу
+        double h = 10;
+	double xpos[N];
+	
+	
+	//створити масив потоків
+	thread myThreads[N];
+	
+	
+	
 	thread t1(foo);
 	thread t2(bar);
 
