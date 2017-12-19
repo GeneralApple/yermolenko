d6487a1f62d6cc3aed703a340fa3d806514f65e3
Ustim93 <Yustim@ukr.net>
2017-12-19 17:33:19 +0300

    Update model.cpp

--- Grygoriev/GPU/model.cpp
+++ Grygoriev/GPU/model.cpp
@@ -7,12 +7,21 @@
 
 using namespace std;
 
-double random()
-{
+double random(int id)
+{	double r = (rand()* (1+1664524*id) + 1013904223UL*id)%RAND_MAX;
 	double r = rand() / (RAND_MAX + 1.0);
+	return rnd0 * (1+1664524*id) + 1013904223UL*id;
 }
 
-void model();
+void model(int j){
+	
+	double x=0, y=0; //початкові координати частинки
+	double l; //відстань прольоту частинки між актами взаємодії
+	//int n = 0;
+	double Theta = 0; //напрям руху частинки
+	double dTh;
+	
+	int n = 0;
 
 
 int main() {
@@ -24,6 +33,9 @@ int main() {
         double mu=0.1; //довжина вільного пробігу
         double h = 10;
 	double xpos[N];
+	
+	double pi = 3.1416;
+	
 	
 	
 	//створити масив потоків
